c1a0e3c7074461034208c88f4bab687bcfa59869
lorenzo <lor90td@gmail.com>
2020-05-14 14:34:58 -0500

    Add project root

---
+++ java-bean-validator/src/main/java/com/lorenzo/java/course/validator/JavaBeanValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.lorenzo.java.course.validator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBeanValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaBeanValidatorApplication.class, args);
+	}
+
+}
---
+++ java-bean-validator/src/test/java/com/lorenzo/java/course/validator/JavaBeanValidatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lorenzo.java.course.validator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBeanValidatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
